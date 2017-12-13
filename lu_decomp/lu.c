2d390648a3573b55d8face252cc55944057fd30b
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-12-13 13:15:21 +0900

    use heap instead of stack

--- lu_decomp/lu.c
+++ lu_decomp/lu.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
 
-#define N 192
-
-double  A[N][N];
-double  b[N];
-double  x[N];
-double  c[N];
-
-void LUsolve(double A[N][N], double b[N], double x[N], int n) 
-{
+void LUsolve(double *A, double *b, double *x, double *c, int n) {
      int i, j, k;
      double dtemp;
 
      /* LU decomposition ---------------------- */  
      for (k=0; k<n; k++) {
-       dtemp = 1.0 / A[k][k];
+       dtemp = 1.0 / A[k * n + k];
        for (i=k+1; i<n; i++) {
-         A[i][k] = A[i][k]*dtemp;   
+         A[i * n + k] = A[i * n + k]*dtemp;   
        }
        for (j=k+1; j<n; j++) {
-         dtemp = A[j][k];
+         dtemp = A[j * n + k];
          for (i=k+1; i<n; i++) {
-           A[j][i] = A[j][i] - A[k][i]*dtemp; 
+           A[j * n + i] = A[j * n + i] - A[k* n + i]*dtemp; 
          }
        }
      }
@@ -34,33 +26,46 @@ void LUsolve(double A[N][N], double b[N], double x[N], int n)
      for (k=0; k<n; k++) {
        c[k] = b[k];
        for (j=0; j<k; j++) {
-         c[k] -= A[k][j]*c[j];
+         c[k] -= A[k * n + j]*c[j];
        }
      }
      /* --------------------------------------- */
 
 
      /* Backward substitution ------------------ */  
-     x[n-1] = c[n-1]/A[n-1][n-1];
+     x[n-1] = c[n-1]/A[(n-1) * n + (n-1)];
      for (k=n-2; k>=0; k--) {
        x[k] = c[k];
        for (j=k+1; j<n; j++) {
-         x[k] -= A[k][j]*x[j];
+         x[k] -= A[k * n + j]*x[j];
        }
-       x[k] = x[k] / A[k][k];
+       x[k] = x[k] / A[k * n + k];
      }
      /* --------------------------------------- */
 }
 
 int main(int argc, char* argv[]) {
+     const int N = 192;
      int i, j, ii;
+
+     double *A = malloc(sizeof(double) * N * N);
+     double *b = malloc(sizeof(double) * N);
+     double *x = malloc(sizeof(double) * N);
+     double *c = malloc(sizeof(double) * N);
+
+     /*
+     memset(A, 0, sizeof(double) * N * N);
+     memset(b, 0, sizeof(double) * N);
+     memset(x, 0, sizeof(double) * N);
+     memset(c, 0, sizeof(double) * N);
+     */
 
      // matrix generation
      for(j=0; j<N; j++) {
        ii = 0;
        for(i=j; i<N; i++) {
-	 A[j][i] = (N-j) - ii;
-	 A[i][j] = A[j][i];
+	 A[j * N + i] = (N-j) - ii;
+	 A[i * N + j] = A[j * N + i];
 	 ii++;
        }
      }
@@ -69,12 +74,12 @@ int main(int argc, char* argv[]) {
      for (i=0; i<N; i++) {
        b[i] = 0.0;
        for (j=0; j<N; j++) {
-         b[i] += A[i][j];
+         b[i] += A[i * N + j];
        }
      }
 
      // solve Ax = b using LU decomposition
-     LUsolve(A, b, x, N);
+     LUsolve(A, b, x, c, N);
 
      // print the answer: x should be (1, 1, 1, ..., 1)
      printf("x: \n");
